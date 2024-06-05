b547e2f5442fc9cbea17902527b6f318447fd11f
Jon Bogert <jonbogert@gmail.com>
2024-06-04 21:25:39 -0700

    Spectator Camera

---
+++ Assets/01_SCRIPTS/Utility/SpectatorCamera.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+/// <summary>
+/// Smooth follow motion for spectator camera
+/// Will destroy itself (and rest of GO) in Android release build
+/// </summary>
+public class SpectatorCamera : MonoBehaviour
+{
+    [SerializeField] Transform _cameraTransform;
+    [Range(0f,1f)]
+    [SerializeField] float _positionSmooth = 0.5f;
+    [Range(0f, 1f)]
+    [SerializeField] float _rotationSmooth = 0.5f;
+
+    private void Awake()
+    {
+#if UNITY_ANDROID
+        if (!Debug.isDebugBuild)
+        {
+            Destroy(gameObject);
+            return;
+        }
+#endif
+
+        if (_cameraTransform == null)
+        {
+            Debug.LogError("SpectatorCamera -> No Camera Transform assigned in inspector");
+            return;
+        }
+    }
+
+    private void OnEnable()
+    {
+        ResetPosition();
+    }
+
+    private void Update()
+    {
+        if (_cameraTransform == null)
+            return;
+
+        transform.position = Vector3.Lerp(_cameraTransform.position, transform.position, _positionSmooth);
+        transform.rotation = Quaternion.Slerp(_cameraTransform.rotation, transform.rotation, _rotationSmooth);
+    }
+
+    private void ResetPosition()
+    {
+        if (transform == null)
+            return;
+
+        transform.position = _cameraTransform.position;
+        transform.rotation = _cameraTransform.rotation;
+    }
+}
